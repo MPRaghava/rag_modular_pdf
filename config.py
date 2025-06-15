48427d39e43ae076221ddd02437311642e1c7c80
Raghava Reddy MP <reddy8.bm@gmail.com>
2025-06-15 11:31:56 +0530

    api key erased.

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY ="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"
+GROQ_API_KEY ="your API Key"
 EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
